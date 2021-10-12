4385c177f266ef92fe531adc4509578bcef22f6d
Lance Martija <jacobmartija03@gmail.com>
2021-10-13 02:16:17 +0800

    feat: onlick function to hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log('hello world!')
+const sidebarMenu = document.querySelector('[data-sidebar-menu]')
+
+document.addEventListener('click', (e) => {
+  const isHamburger = e.target.matches('[data-hamburger-button]')
+  const isCloseButton = e.target.matches('[data-close-button]')
+
+  if (
+    isHamburger ||
+    isCloseButton ||
+    e.target.closest('[data-hamburger-button]') != null ||
+    e.target.closest('[data-close-button]') != null
+  ) {
+    sidebarMenu.classList.toggle('active')
+  }
+})
