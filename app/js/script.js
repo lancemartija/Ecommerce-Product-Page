1be70ab8ad59053549db83378ec23ec30a7c92ef
Lance Martija <jacobmartija03@gmail.com>
2021-10-17 17:45:59 +0800

    feat: no scroll and overlay toggle functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,15 @@
 const sidebarMenu = document.querySelector('[data-sidebar-menu]')
+const overlay = document.querySelector('[data-sidebar-overlay]')
+const body = document.querySelector('body')
 
 document.addEventListener('click', (e) => {
   const isHamburger = e.target.matches('[data-hamburger-button]')
   const isCloseButton = e.target.matches('[data-close-button]')
 
   if (e.target.closest('[data-sidebar-menu]') == null) {
+    body.classList.remove('no-scroll')
     sidebarMenu.classList.remove('active')
+    overlay.classList.remove('overlay')
   }
 
   if (
@@ -14,6 +18,8 @@ document.addEventListener('click', (e) => {
     e.target.closest('[data-hamburger-button]') != null ||
     e.target.closest('[data-close-button]') != null
   ) {
+    body.classList.toggle('no-scroll')
     sidebarMenu.classList.toggle('active')
+    overlay.classList.toggle('overlay')
   }
 })
