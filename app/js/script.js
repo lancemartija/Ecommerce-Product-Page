692c3be4e8eb9ce9fc8909b367730ad1c9aa4c7d
Lance Martija <jacobmartija03@gmail.com>
2021-10-17 18:55:57 +0800

    feat: overhaul hamburger menu toggle script and add animation toggle

--- app/js/script.js
+++ app/js/script.js
@@ -2,24 +2,36 @@ const sidebarMenu = document.querySelector('[data-sidebar-menu]')
 const overlay = document.querySelector('[data-sidebar-overlay]')
 const body = document.querySelector('body')
 
+window.addEventListener('resize', () => {
+  if (this.innerWidth > 767) {
+    body.classList.remove('no-scroll')
+    sidebarMenu.classList.remove('toggle-in')
+    sidebarMenu.classList.remove('toggle-out')
+    overlay.classList.remove('overlay')
+  }
+})
+
 document.addEventListener('click', (e) => {
   const isHamburger = e.target.matches('[data-hamburger-button]')
   const isCloseButton = e.target.matches('[data-close-button]')
+  const sidebarArea = e.target.closest('[data-sidebar-menu]')
+  const hamburgerArea = e.target.closest('[data-hamburger-button]')
+  const closeArea = e.target.closest('[data-close-button]')
 
-  if (e.target.closest('[data-sidebar-menu]') == null) {
-    body.classList.remove('no-scroll')
-    sidebarMenu.classList.remove('active')
-    overlay.classList.remove('overlay')
-  }
-
-  if (
-    isHamburger ||
-    isCloseButton ||
-    e.target.closest('[data-hamburger-button]') != null ||
-    e.target.closest('[data-close-button]') != null
-  ) {
+  if (isHamburger || hamburgerArea != null) {
     body.classList.toggle('no-scroll')
-    sidebarMenu.classList.toggle('active')
+    sidebarMenu.classList.add('toggle-in')
+    sidebarMenu.classList.remove('toggle-out')
     overlay.classList.toggle('overlay')
+  } else if (isCloseButton || closeArea != null) {
+    body.classList.toggle('no-scroll')
+    sidebarMenu.classList.remove('toggle-in')
+    sidebarMenu.classList.add('toggle-out')
+    overlay.classList.toggle('overlay')
+  } else if (sidebarArea == null) {
+    body.classList.remove('no-scroll')
+    sidebarMenu.classList.remove('toggle-in')
+    sidebarMenu.classList.add('toggle-out')
+    overlay.classList.remove('overlay')
   }
 })
